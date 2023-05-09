03ff3b865ca8051e733eed8adb2a6b1bb1798184
b-sandrine <sanrdushimimana@gmail.com>
2023-05-09 14:36:07 +0200

    Added basic page

--- weather-forecast-web/src/App.jsx
+++ weather-forecast-web/src/App.jsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-
+import { MdSearch, MdSunny } from 'react-icons/md'
+import sun from './assets/sun-dynamic-color.png'
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='container'>
+      <img src={sun} alt="sun image" />
+      <div className="weather--part">
+        <div className="search--box">
+          <input type="text" name="" id="" placeholder='Search ...'/>
+          <MdSearch className='icon'/> 
+        </div>
+        <div className="weather--details">
+          <div className="text--upper">
+            <p className='location'>Azerbaijan, Baku</p>
+            <p className='temperature'>13 &#176;C</p>
+            <div className='weather--status'> <p>Sunny</p> <MdSunny className='icon'/> </div>
+          </div>
+          <div className="text--medium">
+            <p className='humidity'>Humidity: 40%</p>
+            <p className="wind--speed">Wind Speed: 13 km/h</p>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
