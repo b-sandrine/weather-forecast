87a8106e9541e16cdca1d135b93f94f164db68f1
b-sandrine <sanrdushimimana@gmail.com>
2023-05-27 14:53:16 +0200

    Added controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,1 +1,13 @@
- 
+const Users = require("../models/users.model");
+
+export const addUser = async (req, res) => {
+  try {
+    const user = req.body;
+    if (user) {
+      const result = await Users.save(user);
+      res.status(201).send("Successfully added a user");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
