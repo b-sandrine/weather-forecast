d440c5be3021bcf0153c31eebdeb1073dc95086e
b-sandrine <sanrdushimimana@gmail.com>
2023-05-27 15:00:15 +0200

    added connection between packages

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const routes = require('./routes/users.routes')
 
 var app = express();
 
@@ -8,6 +9,12 @@ const PORT = 3000;
 mongoose.connect('mongodb://localhost:27017/vehicleTrackingMS')
 .then(console.log('Successfully connected to db'))
 .catch(err => console.error(err))
+
+app.get('/', (req,res) => {
+    res.send('Welcome to Weather forecast backend')
+})
+
+app.use('/api/',routes)
 
 app.listen(PORT, (req,res) => {
     console.log(`app running on http://localhost:${PORT}`);
