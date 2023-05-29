8922d86413f0e9d4bba0aed5eec9d055e721ea51
b-sandrine <sanrdushimimana@gmail.com>
2023-05-29 10:05:53 +0200

    finished login, register, welcom

--- weather-forecast-web/src/App.jsx
+++ weather-forecast-web/src/App.jsx
@@ -1,52 +1,13 @@
 /* eslint-disable no-unused-vars */
 import "./App.css";
-import sun from "./assets/sun-dynamic-color.png";
-import CurrentWeather from "./components/CurrentWeather/CurrentWeather";
-import Search from "./components/search/search";
-import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
-import { useState } from "react";
 import Signup from "./pages/Signup/Signup";
 import Login from "./pages/Login/Login";
 import {Routes, Route } from 'react-router'
 import Welcome from "./pages/Welcome/welcome";
 
 function App() {
-  const [currentWeather, setCurrentWeather] = useState(null);
-  const [forecast, setForecast] = useState(null);
-
-  function handleOnSearchChange(searchData) {
-    const [lat, lon] = searchData.value.split(" ");
-
-    console.log("Latitude is " + lat + " Longitude is " + lon);
-    const currentWeatherFetch = fetch(
-      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
-    );
-    const forecastFetch = fetch(
-      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
-    );
-
-    Promise.all([currentWeatherFetch, forecastFetch])
-      .then(async (response) => {
-        const weatherResponse = await response[0].json();
-        const forecastResponse = await response[1].json();
-
-        setCurrentWeather({ city: searchData.label, ...weatherResponse });
-        setForecast({ city: searchData.label, ...forecastResponse });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-
   return (
     <div className="container">
-      {/* <img src={sun} alt="sun image" />
-      <div className="weather--part">
-        <Search onSearchChange={handleOnSearchChange} />
-        {currentWeather && <CurrentWeather data={currentWeather} />}
-      </div> */}
-      
-
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path='/signup' element={<Signup />} />
--- weather-forecast-web/src/pages/Welcome/welcome.jsx
+++ weather-forecast-web/src/pages/Welcome/welcome.jsx
@@ -1,9 +1,49 @@
+import sun from "../../assets/sun-dynamic-color.png";
+import CurrentWeather from "../../components/CurrentWeather/CurrentWeather";
+import Search from "../../components/search/search";
+import { WEATHER_API_URL, WEATHER_API_KEY } from "../../api";
+import { useState } from "react";
+import './welcome.css'
+
 const Welcome = () => {
-    return (
-        <div className="Welcome--container">
-           <h1> Welcome back, Mr Vincent!</h1>
-        </div>
-    )
-}
+  const [currentWeather, setCurrentWeather] = useState(null);
+  // eslint-disable-next-line no-unused-vars
+  const [forecast, setForecast] = useState(null);
+
+  function handleOnSearchChange(searchData) {
+    const [lat, lon] = searchData.value.split(" ");
+
+    console.log("Latitude is " + lat + " Longitude is " + lon);
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+    );
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+    );
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forecastResponse });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  return (
+    <div className="welcome--container">
+      <h1> Welcome back, Mr Vincent!</h1>
+      <img src={sun} alt="sun image" />
+      <div className="weather--part">
+        <Search onSearchChange={handleOnSearchChange} />
+        {currentWeather && <CurrentWeather data={currentWeather} />}
+      </div>
+    </div>
+  );
+};
 
 export default Welcome;
